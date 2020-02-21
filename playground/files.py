f937c27ae2d3f5c1237bd4ff4b2f935f20772773
ManuelSerna <manuelserna1997@gmail.com>
2020-02-21 01:43:18 -0600

    added write capability to sub directories

--- playground/files.py
+++ playground/files.py
@@ -2,6 +2,10 @@
 # Use JSON as .json files can be read by other programs and is human readable
 
 import json
+import os # able to write to directories
+
+letter = 'A'
+counter = 1
 
 # Sample time series
 x = [1, 2, 3, 4, 5]
@@ -13,14 +17,28 @@ data['y'] = y
 
 # write some arrays to a json file
 #'''
-with open("data.json", 'w') as file:
-    file.write(json.dumps(data, indent=4))
+# Now with added twist of writing to a subdirectory
+here = os.path.dirname(os.path.realpath(__file__))
+subdir = letter
+out_file = "{}{}.json".format(letter, counter)
+file_path = os.path.join(here, subdir, out_file)
+
+# Check subdirectory
+if not os.path.isdir(os.path.join(here, subdir)):
+    os.mkdir(os.path.join(here, subdir)) # make
+
+# Create file
+try:
+    with open(file_path, 'w') as file:
+        file.write(json.dumps(data, indent=4))
+except IOError:
+    print("Wrong path provided")
 #'''
 
 
 
 # Load some arrays from file
-#
+'''
 file_name = "data.json"
 text = open(file_name, 'r')
 text_str = text.read()
